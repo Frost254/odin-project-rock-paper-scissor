e8a2a2778ef8f41cbdf4377bb734956418357b6d
Derrick Nyasio <dnyasio2014@gmail.com>
2023-01-26 10:26:52 +0300

    Done

--- js/index.js
+++ js/index.js
@@ -1,73 +1,97 @@
-const getComputerChoice = () => {
-    let r = Math.floor((Math.random() * 3) + 1);
-    if (r == 1) {
-        return "rock"
-    }
+// Get the buttons
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
 
-    else if (r == 2) {
-        return "paper"
-    }
+const result = document.getElementById("result");
+const scoreboard = document.getElementById("scoreboard");
 
-    else {
-        return "scissor"
+var scoreboardData = {
+    wins: 0,
+    loses: 0,
+    draws: 0,
+    round: 0
+};
+
+// Add event listeners to the buttons
+rockButton.addEventListener("click", function() {
+  playGame("rock");
+});
+paperButton.addEventListener("click", function() {
+  playGame("paper");
+});
+scissorsButton.addEventListener("click", function() {
+  playGame("scissors");
+});
+
+// Play the game
+function playGame(playerChoice) {
+  // Get the computer's choice
+  const computerChoice = getComputerChoice();
+
+  // Compare the choices and determine the winner
+  const gameResult = getGameResult(playerChoice, computerChoice);
+
+  // Update the scoreboard
+  updateScoreboard(gameResult,playerChoice,computerChoice);
+
+  // Display the result
+  result.innerHTML = `You chose ${playerChoice}, the computer chose ${computerChoice}. ${gameResult}.`;
+}
+
+// Get the computer's choice
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+// Compare the choices and determine the winner
+function getGameResult(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    scoreboardData.draws++;
+    return "It's a draw!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    scoreboardData.wins++;
+    return "You won!";
+  } else {
+    scoreboardData.loses++;
+    return "The computer won!";
+  }
+}
+
+function updateScoreboard(result, playerChoice, computerChoice) {
+    var newRow = scoreboard.insertRow(-1);
+    var roundCell = newRow.insertCell(0);
+    var playerCell = newRow.insertCell(1);
+    var computerCell = newRow.insertCell(2);
+    var outcomeCell = newRow.insertCell(3);
+    roundCell.innerHTML = ++scoreboardData.round;
+    playerCell.innerHTML = playerChoice;
+    computerCell.innerHTML = computerChoice;
+    outcomeCell.innerHTML = result;
+    if (result == "You won!") {
+        scoreboardData.wins++;
+    }
+    else if (result == "The computer won!") {
+        scoreboardData.loses++;
+    }
+    else{
+        scoreboardData.draws++;
+    }
+    document.getElementById("wins").innerHTML = scoreboardData.wins;
+    document.getElementById("loses").innerHTML = scoreboardData.loses;
+    document.getElementById("draws").innerHTML = scoreboardData.draws;
+    if (scoreboardData.wins === 5) {
+        alert("You won! Congratulations!");
+    }
+    else if (scoreboardData.loses === 5) {
+        alert("The computer won! Better luck next time!");
     }
 }
 
-let computerRoundCount = 0;
-let playerRoundCount = 0;
-
-function playRound (playerSelection, computerSelection) {
-    let ps = playerSelection.toLowerCase()
-    let cs = computerSelection.toLowerCase()
-
-
-    if (ps === "rock" && cs === "scissor") {
-        playerRoundCount += 1;
-        return `The player wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === "rock" && cs === "paper") {
-        computerRoundCount += 1;
-        return `The computer wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === "paper" && cs === "scissor") {
-        computerRoundCount += 1;
-        return `The computer wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === "paper" && cs === "rock") {
-        playerRoundCount += 1;
-        return `The player wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === "scissor" && cs === "paper") {
-        playerRoundCount += 1;
-        return `The player wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === "scissor" && cs === "rock") {
-        computerRoundCount += 1;
-        return `The computer wins! You chose ${ps} and the computer chose ${cs}`
-    }
-
-    else if (ps === cs) {
-        return "The round ends in a draw! You both picked " + ps
-    }
-
-    else {
-        return "Invalid player selection, you picked " + ps;
-    }
-}
-
-function game () {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What's your pick? ");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`The player has ${playerRoundCount} rounds and the computer has ${computerRoundCount} rounds.`)
-    }
-}
-
-game()
 
